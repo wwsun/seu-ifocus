c906fe23a768d394d621754a4b763b0ff049499f
Weiwei SUN <ww.sun@outlook.com>
2015-03-17 21:49:11 +0800

    update

--- public/js/directive/pathchart.drct.js
+++ public/js/directive/pathchart.drct.js
@@ -1,15 +1,20 @@
 angular.module('chart.path', []).directive('pathChart', pathChart);
 
+// todo: handle the click event
+// todo: resize to change the view level
+
 function pathChart() {
     function link(scope, el, attr) {
 
         var energy = {"nodes":[{"url":"2837","name":0},{"url":"6030","name":1},{"url":"2582","name":2},{"url":"418","name":3},{"url":"486","name":4},{"url":"7633","name":5},{"url":"1195","name":6},{"url":"5380","name":7},{"url":"1775","name":8},{"url":"3718","name":9},{"url":"4382","name":10},{"url":"6624","name":11},{"url":"7633","name":12}],"links":[{"value":25,"source":0,"target":1},{"value":1,"source":1,"target":2},{"value":1,"source":1,"target":3},{"value":1,"source":1,"target":4},{"value":20,"source":1,"target":5},{"value":1,"source":2,"target":6},{"value":1,"source":3,"target":7},{"value":1,"source":4,"target":8},{"value":1,"source":5,"target":9},{"value":1,"source":5,"target":10},{"value":2,"source":5,"target":11},{"value":8,"source":5,"target":12}]};
 
-        var margin = {top: 1, right: 1, bottom: 6, left: 1};
+        var margin = {top: 1, right: 1, bottom: 6, left: 1},
+            width = 800,
+            height = 700;
 
         var formatNumber = d3.format(",.0f"),
             format = function (d) {
-                return formatNumber(d) + " s";
+                return "weight " + formatNumber(d);
             },
             color = d3.scale.category20();
 
@@ -94,6 +99,13 @@ function pathChart() {
             })
             .attr("x", 6 + sankey.nodeWidth())
             .attr("text-anchor", "start");
+
+        node.on('click', nodeClickHandler);
+
+        function nodeClickHandler(d) {
+            // d is the data item for the current data/element pair
+            console.log("url id: " + d.url);
+        }
 
         function dragmove(d) {
             d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
