deb7698059cc8def16ee5319d6ebe2a4392c921d
Weiwei SUN <ww.sun@outlook.com>
2015-03-21 16:51:48 +0800

    adding angular-chart.js support

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ angular.module('sampleApp', [
     'ui.router',
     'appRoutes',
 
+    'chart.js',
+
     // --- DIRECTIVES ---
     'chart.pie',
     'chart.path',
@@ -12,6 +14,7 @@ angular.module('sampleApp', [
 
     // --- CONTROLLERS ---
     'MainCtrl',
+    'StatusCtrl',
     'AdminCtrl',
     'GeekCtrl'
 
