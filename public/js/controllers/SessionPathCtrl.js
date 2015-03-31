07e717ad2f8233727898b0ff02a9b6ff3d032549
Weiwei SUN <ww.sun@outlook.com>
2015-03-31 16:46:00 +0800

    update

--- public/js/controllers/SessionPathCtrl.js
+++ public/js/controllers/SessionPathCtrl.js
@@ -2,7 +2,7 @@
  * Created by Weiwei on 2015/3/31.
  */
 
-angular.module('SessionCtrl', []).controller('SessionPathController', function(graphData) {
+angular.module('SessionCtrl', []).controller('SessionPathController', function (graphData) {
 
     var vm = this;
     vm.$inject = ['graphData'];
@@ -21,4 +21,11 @@ angular.module('SessionCtrl', []).controller('SessionPathController', function(g
         in_degree: 1,
         out_degree: 1
     };
+
+    vm.highDropOff = [
+        {url: "www.made-in-china.com", drop: 60},
+        {url: "www.made-in-china.com/search", drop: 55},
+        {url: "www.made-in-china.com/category", drop: 50},
+        {url: "www.made-in-china.com/product", drop: 40}
+    ];
 });
