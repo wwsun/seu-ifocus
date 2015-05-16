8372797d51f1202c94b1bb0c14675e8ca54fa92f
Weiwei SUN <ww.sun@outlook.com>
2015-05-16 21:54:02 +0800

    traffic conversion module

--- public/js/traffic/conversion.controller.js
+++ public/js/traffic/conversion.controller.js
@@ -4,5 +4,16 @@ angular.module('ifocus.traffic')
 
         var vm = this;
 
+        vm.inquiryTrends = {
+            labels: [0,2,4,6,8,10,12,14,16,18,20,22,24],
+            series: ['Inquiry Rate'],
+            data: [[20,30,40,50,30,20,60,20,20,10,20,50,10]]
+        };
+
+        vm.inquiryNumbers = {
+            total: 5630,
+            inquiry: 300,
+            rate: 5.4
+        };
 
     });
