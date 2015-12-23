f21d54ad3ae96fb7b1df41cfff94550ce34c351b
Weiwei SUN <ww.sun@outlook.com>
2015-12-23 17:04:53 +0800

    Adjust layouts for replay and event page

--- public/js/session/sessionreplay.controller.js
+++ public/js/session/sessionreplay.controller.js
@@ -1,13 +1,34 @@
 angular.module('ifocus.sessionController', [])
 
-    .controller('SessionReplayCtrl', ['SessionService', function (SessionService) {
-        var vm = this;
+  .controller('SessionReplayCtrl', ['SessionService', function (SessionService) {
+    var vm = this;
 
-        SessionService.getSessionSequenceByID()
-            .success(function (data) {
+    SessionService.getSessionSequenceByID()
+      .success(function (data) {
 
-                vm.sessionSequence = data;
+        console.log(data);
 
-            });
+        var result = data.map(function (item) {
 
-    }]);
+          if (item.url === '-') return item;
+
+          item.product = '';
+          item.product = item.url.split('/')[2].split('.')[0];
+
+          return item;
+        });
+
+        vm.sessionSequence = result;
+
+      });
+
+    vm.pageDetailByUrl = {
+      labels: ["服务", "评价", "价格", "商品描述"],
+      data: {
+        click: [20, 30, 40, 10],
+        hover: [30, 40, 15, 15]
+      }
+
+    };
+
+  }]);
